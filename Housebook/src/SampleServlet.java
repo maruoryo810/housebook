e240602841ad35dee5075224be76fa802f20af4f
ryo maruo <r.ryou1711@morijyobi.ac.jp>
2018-07-09 11:48:46 +0900

    さよなら

--- Housebook/src/SampleServlet.java
+++ Housebook/src/SampleServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 public class SampleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-
+//こんにちは
     /**
      * @see HttpServlet#HttpServlet()
      */
